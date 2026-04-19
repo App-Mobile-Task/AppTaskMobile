390360d4dff8d9cabf7eb67f88803d4f7cab29f1
Jppcamilo <jpcamilopereira@gmail.com>
2026-04-19 17:23:25 -0300

    feat: conectar rotas de navegação no arquivo raiz App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import MainTabs from './src/screens/MainTabs';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MainTabs/>
+       <StatusBar style="auto" />
     </View>
   );
 }
